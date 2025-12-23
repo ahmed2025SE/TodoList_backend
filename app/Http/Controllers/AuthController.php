f406bd37f1b833947db02ed99c2d41ec75a56734
Ahmed badr <ahmedweb2025site@gmail.com>
2025-12-23 12:03:22 +0200

    Add authentication (signup & login) endpoints

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+public function signup(Request $request)
+{
+    $inputs = $request->validate([
+        'name'     => ['required', 'string', 'max:255'],
+        'username' => ['required', 'string', 'min:4', 'max:20', 'unique:users'],
+        'email'    => ['required', 'email', 'unique:users'],
+        'password' => ['required', 'min:8'],// سوف يتم تشفيرها تلقائيًا في كلاس المستخدم
+    ]);
+
+    User::create($inputs);
+
+    return response()->json([
+        'message' => 'User registered successfully'
+    ], 201);
+}
+
+
+    public function login(Request $request)
+    {
+         $inputs = $request->validate([
+        'username' => ['required', 'string', 'max:255'],
+        'password' => ['required', 'min:8'],
+        ]);
+
+
+         $user = User::where('username', $inputs['username'])->first();
+
+        if (!$user || !Hash::check($inputs['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user'  => $user,
+            'token' => $token
+        ]);
+    }
+
+}
