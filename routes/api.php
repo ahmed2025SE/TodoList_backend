d4715c5c43ef8f414a9268775fa203c23121ba33
Ahmed badr <ahmedweb2025site@gmail.com>
2025-12-25 22:31:11 +0200

    Add TodoList CRUD controller, model, migration and API routes

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\TodoListController;
 
 
 
-// تسجيل حساب جديد
+
+
    Route::post('/signup', [AuthController::class, 'signup']);
-// تسجيل الدخول
    Route::post('/login', [AuthController::class, 'login']);
+
+
+   Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('lists', TodoListController::class);
+
+});
